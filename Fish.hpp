3207f31485fa8d56fa2873c760b421824590d1d9
Charlie Hanacek <charlie@czhanacek.com>
2018-03-24 16:58:04 -0700

    fixed image size

--- Fish.hpp
+++ Fish.hpp
@@ -59,7 +59,7 @@ class Fish : public PriDrawable {
         this->type = type;
         if(type == 1) {
             sf::Image img;
-            img.loadFromFile("./images/fish/fish1.png");
+            img.loadFromFile(std::string("./images/fish/fish1.png").c_str());
             for(int y = 0; y < img.getSize().y; y++) {
                 int x = 0;
                 while(x < img.getSize().x) {
